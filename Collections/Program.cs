ae22ea8c3f8901e19c08343b87ad9f31f72f95af
Seltenheit <104844876+Seltenheit@users.noreply.github.com>
2022-05-06 22:40:50 +0300

    moving figures

---
+++ Collections/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tetris;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          
+            //int[] nums1 = new int[5];
+            //nums1[0] = 1;
+            //nums1[1] = 2;
+
+            //foreach(int i in nums1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //for(int i = 0; i < nums1.Length; i++)
+            //{
+            //    Console.WriteLine(nums1[i]);
+            //}
+
+            Point[] points = new Point[3];
+            points[0] = new Point(1, 2, '*');
+            points[1] = new Point(3, 4, '#');
+            points[2] = new Point(4, 5, '*');
+
+            foreach(Point p in points)
+            {
+                p.Draw();
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tetris/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public class Point
+    {
+        public int x;
+        public int y;
+
+        public char c;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(c);
+        }
+
+        public Point(int a, int b, char sym)
+        {
+            x = a;
+            y = b;
+            c = sym;
+        }
+        public Point() { }
+
+        internal void Move(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.LEFT:
+                    x -= 1;
+                    break;
+                case Direction.RIGHT:
+                    x += 1;
+                    break;
+                case Direction.DOWN:
+                    y += 1;
+                    break;
+            }
+        }
+    }
+}
