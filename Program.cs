55ccffe547fd4373a5a97094d5327b62bafba264
Seltenheit <104844876+Seltenheit@users.noreply.github.com>
2022-05-25 12:40:59 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Tetris
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(40, 30);
+            Console.SetBufferSize(40, 30);
+
+
+            FigureGenerator generator = new FigureGenerator(20, 0, '*');
+            Figure currentFigure = generator.GenerateNewFigure();
+
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    /*ConsoleKeyInfo*/
+                    var key = Console.ReadKey();
+                    HandleKey(currentFigure, key);
+
+                }
+            }
+        }
+
+        private static void HandleKey(Figure currentFigure, ConsoleKeyInfo key)
+        {
+            switch (key.Key)
+            {
+                case ConsoleKey.LeftArrow:
+                    currentFigure.TryMove(Direction.LEFT);
+                    break;
+                case ConsoleKey.RightArrow:
+                    currentFigure.TryMove(Direction.RIGHT);
+                    break;
+                case ConsoleKey.DownArrow:
+                    currentFigure.TryMove(Direction.DOWN);
+                    break;
+                case ConsoleKey.Spacebar:
+                    currentFigure.TryRotate();
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+}
